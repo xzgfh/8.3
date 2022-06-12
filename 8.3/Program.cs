918e5448116b0351daf28b703489ff685f3e3803
xzgfh <User@WIN-Q06VGDO4GD9>
2022-06-12 15:33:17 +0300

    Добавьте файлы проекта.

---
+++ 8.3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace nr_8._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            str S = new str();
+
+            string s1 = "Hello World";
+            S.letter(s1);
+
+            string s2 = "one, two, three, four, five";
+            S.word(s2);
+
+            string s3 = "Я себе внутривенно вкалываю музыку. Тихо, без боли, поражает каждую мускулу. Это лучше, даже самого чистого воздуха. До слез пробивает и становится глухо.";
+            S.sentence(s3);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 8.3/str.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace nr_8._3
+{
+    class str
+    {
+
+        public void letter(string s1)
+        {
+            foreach (char element in s1)
+            {
+                Console.WriteLine(element);
+            }
+        }
+
+        public void word(string s2)
+        {
+            char[] delimiterChars = { ' ', '?', ',', '.', '/', ':', ';', '!', '+', '-' };
+            string[] words = s2.Split(delimiterChars);
+            foreach (string element in words)
+            {
+                Console.WriteLine(element);
+            }
+
+        }
+
+        public void sentence(string s3)
+        {
+            char delimiterChars = '.';
+            string[] words = s3.Split(delimiterChars);
+            foreach (string element in words)
+            {
+                Console.WriteLine(element);
+            }
+
+        }
+    }
+}
